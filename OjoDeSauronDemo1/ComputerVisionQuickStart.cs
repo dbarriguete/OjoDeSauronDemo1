f1cbd2292e8d0d5d18d12a90b1cea4d184f55a79
Diego Arturo Barriguete <dbarriguete@gmail.com>
2020-12-12 18:21:27 -0600

    Updating code for only consider "persons"

--- OjoDeSauronDemo1/ComputerVisionQuickStart.cs
+++ OjoDeSauronDemo1/ComputerVisionQuickStart.cs
@@ -17,10 +17,8 @@ namespace OjoDeSauronDemo1
         static string subscriptionKey = "";
         static string endpoint = "";
 
-        // URL image used for analyzing an image (image of puppy)
+        // URL image used for analyzing an image (image of puppy) this is for the case when you want to use an image on internet
         private const string ANALYZE_URL_IMAGE = "https://moderatorsampleimages.blob.core.windows.net/samples/sample16.png";
-        private const string LOCAL_IMAGE = "\\img\\Ejemplo01.png";
-//        private const string ANALYZE_URL_IMAGE = "https://github.com/dbarriguete/OjoDeSauronDemo1/blob/main/OjoDeSauronDemo1/img/Ejemplo01.png";
 
         public int ReviewPersons()
         {
@@ -28,11 +26,10 @@ namespace OjoDeSauronDemo1
             //create a client
             ComputerVisionClient client = Authenticate(endpoint, subscriptionKey);
 
-            string imagelocation = Path.Combine(Path.GetPathRoot( Directory.GetCurrentDirectory() ),  "temp", "img", "Ejemplo03.png");
+            string imagelocation = Path.Combine(Path.GetPathRoot( Directory.GetCurrentDirectory() ),  "temp", "img", "Ejemplo05.PNG");
 
             // Analyze an image to get features and other properties.
             AnalyzeImageUrl(client, imagelocation, false).Wait();
-
 
             return answer;
         }
@@ -97,7 +94,7 @@ namespace OjoDeSauronDemo1
             {
                 Console.WriteLine($"{obj.ObjectProperty} with confidence {obj.Confidence} at location {obj.Rectangle.X}, " +
                   $"{obj.Rectangle.X + obj.Rectangle.W}, {obj.Rectangle.Y}, {obj.Rectangle.Y + obj.Rectangle.H}");
-                if ( obj.ObjectProperty.Equals("person") )
+                if ( obj.ObjectProperty.Equals("person") ) // you should add a validation for a minimum value of obj.Confidence
                 {
                     answer++;
                 }
